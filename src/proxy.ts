3ef09d760b3632ef8633ccc48942206247b5d725
권새롬 <rhdtls3562@naver.com>
2026-04-18 19:26:19 +0900

    ♻️ Refactor : 기능 구현 전까지 리다이렉트 기능 비활성화

--- src/proxy.ts
+++ src/proxy.ts
@@ -7,12 +7,13 @@ export default withAuth({
 });
 
 export const config = {
+  // TODO: UI 구현 완료 후 인증/API 연결 단계에서 보호 라우트 matcher를 복구한다.
   matcher: [
-    '/addteam',
-    '/jointeam',
-    '/myhistory',
-    '/mypage',
-    '/boards/:path*',
-    '/((?!$|login|signup|oauth|api|_next/static|_next/image|favicon.ico|reset-password|password-reset).*)',
+    // '/addteam',
+    // '/jointeam',
+    // '/myhistory',
+    // '/mypage',
+    // '/boards/:path*',
+    // '/((?!$|login|signup|oauth|api|_next/static|_next/image|favicon.ico|reset-password|password-reset).*)',
   ],
 };
