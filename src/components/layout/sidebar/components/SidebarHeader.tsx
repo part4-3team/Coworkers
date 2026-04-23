9169223dd54b12b67655dc029e5587a9b7b189b9
Kwon Saerom <rhdtls3562@naver.com>
2026-04-23 18:38:46 +0900

    Refactor SidebarHeader to use updated ROUTES import

--- src/components/layout/sidebar/components/SidebarHeader.tsx
+++ src/components/layout/sidebar/components/SidebarHeader.tsx
@@ -12,7 +12,7 @@ import {
   imgLogoSymbolLarge,
 } from '@/assets';
 import type { SidebarHeaderProps } from '@/components/layout/sidebar/types';
-import { ROUTES } from '@/constants/routes';
+import { ROUTES } from '@/constants/ROUTES';
 import { cn } from '@/utils/cn';
 
 export default function SidebarHeader({
@@ -27,18 +27,22 @@ export default function SidebarHeader({
       )}
     >
       <Link href={ROUTES.HOME} aria-label="랜딩 페이지로 이동">
-        <Image
-          src={imgLogoSymbolLarge}
-          alt="Coworkers"
-          className={cn('h-6 w-auto', isExpanded && 'hidden')}
-          priority
-        />
-        <Image
-          src={imgLogoFullLarge}
-          alt="Coworkers"
-          className={cn('hidden h-6 w-auto', isExpanded && 'block')}
-          priority
-        />
+        {isExpanded ? (
+          <Image
+            src={imgLogoFullLarge}
+            alt="Coworkers"
+            width={144}
+            height={24}
+            loading="eager"
+          />
+        ) : (
+          <Image
+            src={imgLogoSymbolLarge}
+            alt="Coworkers"
+            width={35}
+            height={24}
+          />
+        )}
       </Link>
 
       <button
