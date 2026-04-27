3290f0206bd7bca16a7349454e0c41966a4cd3f4
양성준 <111010564+jakejun98@users.noreply.github.com>
2026-04-27 20:11:43 +0900

    🎨 Style: 모바일 토스트 하단 위치 조정

--- src/components/common/toast/ToastProvider.tsx
+++ src/components/common/toast/ToastProvider.tsx
@@ -67,7 +67,7 @@ export function ToastProvider({ children }: { children: React.ReactNode }) {
     <ToastContext.Provider value={contextValue}>
       {children}
       {hasToasts && (
-        <div className="fixed bottom-28 left-1/2 z-50 flex -translate-x-1/2 flex-col gap-2">
+        <div className="fixed bottom-16 left-1/2 z-50 flex -translate-x-1/2 flex-col gap-2 md:bottom-28">
           {toasts.map((toast) => (
             <Toast key={toast.id} toast={toast} onRemove={removeToast} />
           ))}
