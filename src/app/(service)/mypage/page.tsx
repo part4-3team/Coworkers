74b46d30c15d7c4669204ca112acb1ca54d143ad
Song <ziy1027@naver.com>
2026-04-28 21:53:47 +0900

    ✨ Feat: 추가: page.tsx 서버 컴포넌트로 변경/ 클라이언트 컴포넌트 따로 구성

--- src/app/(service)/mypage/page.tsx
+++ src/app/(service)/mypage/page.tsx
@@ -1,57 +1,7 @@
-/**
- * 계정 설정 페이지를 구성하는 파일입니다.
- */
-'use client';
-import { useState } from 'react';
+import MyPageClient from '@/app/(service)/mypage/components/MyPageClient';
 
-import Image from 'next/image';
-
-import AccountForm from '@/app/(service)/mypage/components/AccountForm';
-import WithdrawModal from '@/app/(service)/mypage/components/WithdrawModal';
-import { icLogout } from '@/assets/index';
-import { PrimaryButton } from '@/components/common/button';
+export const metadata = { title: '계정 설정' };
 
 export default function MyPage() {
-  const [isWithdrawModalOpen, setIsWithdrawModalOpen] = useState(false);
-
-  const [isDirty, setIsDirty] = useState(false);
-
-  return (
-    <div className="w-full h-full min-h-dvh flex justify-center items-center gap-4 flex-wrap px-4 py-6 md:px-16 md:py-10">
-      <div className="bg-background-inverse px-5.5 pt-12 pb-16 rounded-[20px] flex flex-col gap-8 w-full md:px-11 md:pt-16  xl:max-w-235 xl:px-14">
-        <h2 className="text-text-primary text-[20px] font-bold">계정 설정</h2>
-        <AccountForm isDirty={isDirty} onDirtyChange={setIsDirty} />
-        <div className="mt-1">
-          <div className="flex justify-end">
-            <button
-              onClick={() => setIsWithdrawModalOpen(true)}
-              className="flex gap-2 text-status-danger font-medium text-base"
-            >
-              <Image
-                src={icLogout}
-                width={15.75}
-                height={12}
-                alt="회원 탈퇴 아이콘"
-              />
-              회원 탈퇴하기
-            </button>
-          </div>
-          <div className="flex justify-center items-center pt-10 m-w-70 m-auto w-full">
-            <PrimaryButton form="accountForm" type="submit" disabled={!isDirty}>
-              변경하기
-            </PrimaryButton>
-          </div>
-          {/* 소셜 회원 확인 후 노출 될 문구 */}
-          <div className="flex justify-center items-center px-6 pt-10">
-            <p className="text-brand-primary text-[15px] font-medium">
-              소셜 로그인 회원입니다.
-            </p>
-          </div>
-          {isWithdrawModalOpen && (
-            <WithdrawModal onClose={() => setIsWithdrawModalOpen(false)} />
-          )}
-        </div>
-      </div>
-    </div>
-  );
+  return <MyPageClient />;
 }
