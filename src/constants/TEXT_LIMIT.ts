697fce71308772746a9e02784b0832632104f04d
Song <ziy1027@naver.com>
2026-05-16 10:49:27 +0900

    🐛 Fix : 게시글 추가/수정에도 글자 제한수 추가

--- src/constants/TEXT_LIMIT.ts
+++ src/constants/TEXT_LIMIT.ts
@@ -5,3 +5,6 @@
 export const TITLE_TEXT_LIMIT = 30;
 export const MEMO_TEXT_LIMIT = 255;
 export const TASKLIST_TEXT_LIMIT = 15;
+
+export const ARTICLE_TITLE_TEXT_LIMIT = 100;
+export const ARTICLE_CONTENT_TEXT_LIMIT = 1000;
